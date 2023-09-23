079f6548a41c556e9f11be2e4e2717212a78f304
Mehedy-Tanvir <mehedytanvir451@gmail.com>
2023-09-23 21:02:02 +0600

    applied job filter added

--- src/Components/AppliedJobs/AppliedJobs.jsx
+++ src/Components/AppliedJobs/AppliedJobs.jsx
@@ -5,11 +5,28 @@ import { getStoredJobApplication } from "../../Utility/LocalStorage";
 const AppliedJobs = () => {
   const jobs = useLoaderData();
   const [appliedJobs, setAppliedJobs] = useState([]);
+  const [displayJobs, setDisplayJobs] = useState([]);
+  const handleJobsFilter = (filter) => {
+    if (filter === "all") {
+      setDisplayJobs(appliedJobs);
+    } else if (filter === "remote") {
+      const remoteJobs = appliedJobs.filter(
+        (job) => job.remote_or_onsite === "Remote"
+      );
+      setDisplayJobs(remoteJobs);
+    } else if (filter === "onsite") {
+      const onsiteJobs = appliedJobs.filter(
+        (job) => job.remote_or_onsite === "Onsite"
+      );
+      setDisplayJobs(onsiteJobs);
+    }
+  };
   useEffect(() => {
     const storedJobIds = getStoredJobApplication();
     if (jobs.length > 0) {
       const jobsApplied = jobs.filter((job) => storedJobIds.includes(job.id));
       setAppliedJobs(jobsApplied);
+      setDisplayJobs(jobsApplied);
     }
   }, [jobs]);
 
@@ -22,19 +39,19 @@ const AppliedJobs = () => {
       <details className="mb-32 dropdown">
         <summary className="m-1 btn">open or close</summary>
         <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
-          <li>
+          <li onClick={() => handleJobsFilter("all")}>
             <a>All</a>
           </li>
-          <li>
+          <li onClick={() => handleJobsFilter("remote")}>
             <a>Remote</a>
           </li>
-          <li>
+          <li onClick={() => handleJobsFilter("onsite")}>
             <a>Onsite</a>
           </li>
         </ul>
       </details>
       <ul>
-        {appliedJobs.map((job, idx) => (
+        {displayJobs.map((job, idx) => (
           <li key={idx}>
             {idx + 1}. {job.job_title} - {job.company_name} (
             {job.remote_or_onsite})
